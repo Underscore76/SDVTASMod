c7c0ea01b143ee5e64ed2797bc0f609d0e9559e2
Underscore76 <theunderscore76@gmail.com>
2023-07-29 14:09:01 -0500

    speedup fast reset/load and some guards on inputs

--- TASMod/Controller.cs
+++ TASMod/Controller.cs
@@ -209,34 +209,6 @@ namespace TASMod
             state.UpdateGame();
         }
 
-        public static void SaveEngineState(string engine_name = "default_engine_state")
-        {
-            EngineState state = new EngineState();
-            string filePath = Path.Combine(Constants.BasePath, string.Format("{0}.json", engine_name));
-            using (StreamWriter file = File.CreateText(filePath))
-            {
-                JsonSerializer serializer = new JsonSerializer();
-                serializer.Formatting = Formatting.Indented;
-                serializer.Serialize(file, state);
-            }
-        }
-
-        public static void LoadEngineState(string engine_name = "default_engine_state")
-        {
-            string filePath = Path.Combine(Constants.BasePath, string.Format("{0}.json", engine_name));
-            if (!File.Exists(filePath))
-                return;
-
-            EngineState state = null;
-            using (StreamReader file = File.OpenText(filePath))
-            {
-                JsonSerializer serializer = new JsonSerializer();
-                // TODO: any safety rails for overwriting current State?
-                state = (EngineState)serializer.Deserialize(file, typeof(EngineState));
-            }
-            state.UpdateGame();
-        }
-
         public static TASMouseState LastFrameMouse()
         {
             if (TASDateTime.CurrentFrame == 0)
