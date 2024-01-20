9cf3a38e665896d6db8daafe8085fdb9ebefcb6a
Underscore76 <theunderscore76@gmail.com>
2024-01-20 09:59:34 -0600

    replay mode and pause controls

--- TASMod/Extensions/GameRunner.cs
+++ TASMod/Extensions/GameRunner.cs
@@ -132,13 +132,18 @@ namespace TASMod.Extensions
             {
                 try
                 {
+                    if ((int)TASDateTime.CurrentFrame + 2 > Controller.PauseFrame && Controller.GameMode == TASMode.Replay)
+                    {
+                        counter = 0;
+                        break;
+                    }
                     //ModEntry.Console.Log($"Reset {TASDateTime.CurrentFrame}", LogLevel.Error);
                     //runner.Step();
                     GameTime gameTime = TASDateTime.CurrentGameTime;
                     runner.InvokeUpdate(gameTime);
                     runner.InvokeDraw(gameTime);
                     runner.EventLoop();
-                    if (counter++ >= Controller.FramesBetweenRender)
+                    if (counter++ >= Controller.FramesBetweenRender && (Controller.GameMode == TASMode.Edit || !Controller.IsPaused))
                         break;
                 } catch
                 {
@@ -148,7 +153,7 @@ namespace TASMod.Extensions
             }
             //ModEntry.Console.Log($"CurrentFrame: {TASDateTime.CurrentFrame}", LogLevel.Warn);
             TASSpriteBatch.Active = true;
-            if ((int)TASDateTime.CurrentFrame < Controller.State.Count - finalFrames)
+            if (counter >= Controller.FramesBetweenRender)
                 Controller.FastAdvance = true;
         }
 
